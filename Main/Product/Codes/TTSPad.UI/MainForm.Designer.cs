5fa0db1f6ecb8c9b75debf6f0f20fcbfc4fc0cdd
nbxx <SND\nbxx_cp>
2009-09-24 09:08:29 +0000

    change text length to int.MaxValue

--- Main/Product/Codes/TTSPad.UI/MainForm.Designer.cs
+++ Main/Product/Codes/TTSPad.UI/MainForm.Designer.cs
@@ -414,7 +414,7 @@
             this.textBoxMainText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBoxMainText.HideSelection = false;
             this.textBoxMainText.Location = new System.Drawing.Point(0, 24);
-            this.textBoxMainText.MaxLength = 2147483647;
+            this.textBoxMainText.MaxLength = int.MaxValue;
             this.textBoxMainText.Multiline = true;
             this.textBoxMainText.Name = "textBoxMainText";
             this.textBoxMainText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
